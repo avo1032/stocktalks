4c562635b211f0b84984a0ff7366d923c972157e
avo1032 <swan1032@naver.com>
2024-01-25 10:23:09 +0900

    refactor: UserEntity date column

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'user' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  profile_image: string;
+
+  @Column()
+  comment: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
